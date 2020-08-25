7c7daedfdd5e96f94b9b43b590f73019c279b6fb
Rishabhsp98 <rish6501@gmail.com>
2020-08-26 00:22:26 +0530

    coin change update

--- Recursion/CoinChangeAll.cpp
+++ Recursion/CoinChangeAll.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+//Faith
 //Always make 4 call(4 elements here) for every call after reducing every target(every target call will start from 0 to nums.size() using all elements)
 int CoinchangPermu_Inf(vector<int> &nums, int target, string ans)
 {
@@ -52,12 +53,40 @@ int CoinchangeCombina_Single(vector<int> &nums, int target, int idx, string ans)
 
     return count;
 }
+
+/*Just same way like we have to make all 4 numsbers(here) call for every target
+ but now as we can not use same element twice we just block it after making call 
+ for particular target ,and after target==0 (backtrack) we restore it to positive
+for other calls.*/
+
+int CoinchangePermu_Single(vector<int> &nums, int target, string ans)
+{
+    if (target == 0)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+
+    int count = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int temp = nums[i];
+        if (nums[i] > 0 && target - nums[i] >= 0)
+        {
+            nums[i] *= -1;
+            count += CoinchangePermu_Single(nums, target - temp, ans + to_string(temp) + " ");
+            nums[i] *= -1;
+        }
+    }
+    return count;
+}
 void CoinchangeMain()
 {
-    vector<int> arr{1, 2, 5};
+    vector<int> arr{2, 3, 5, 7};
     // cout << "Total  " << CoinchangPermu_Inf(arr, 10, "");
-    cout << "Total  " << CoinchangeCombina_Inf(arr, 5, 0, "");
+    // cout << "Total  " << CoinchangeCombina_Inf(arr, 5, 0, "");
     // cout << "Total  " << CoinchangeCombina_Single(arr, 10, 0, "");
+    cout << "Total  " << CoinchangePermu_Single(arr, 10, "");
 }
 void solve()
 {
